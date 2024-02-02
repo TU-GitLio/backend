6c601b4c2c36c0319a1d198ef724ecc9663dcc05
Kevin Kim <kevinkim0617kim@gmail.com>
2024-02-02 00:18:20 -0800

    Deploy - deploy to fly.io & add sample api

--- gitlio/app/server.py
+++ gitlio/app/server.py
@@ -1,17 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
+from langchain.chat_models import ChatAnthropic, ChatOpenAI
 from langserve import add_routes
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+app = FastAPI(
+    title="LangChain Server",
+    version="1.0",
+    description="Spin up a simple api server using Langchain's Runnable interfaces",
+)
 
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
+@app.get("/template/{name}", response_class=HTMLResponse)
+async def read_item(request: Request, name: str):
+    return templates.TemplateResponse(
+        request=request, name="hello.html", context={"name": name}
+)
 
-# Edit this to add the chain you want to add
-# add_routes(app, NotImplemented)
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/openai",
+)
 
 if __name__ == "__main__":
     import uvicorn
