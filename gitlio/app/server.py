11acd1cc2f28f8e55e74cba8af8d5b273627a4b8
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-04 17:53:56 +0900

    chore: S3 setup & profile upload API

--- gitlio/app/server.py
+++ gitlio/app/server.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request, Depends, HTTPException
+import shutil
+import os
+from fastapi import FastAPI, Request, Depends, HTTPException, UploadFile, File, Response
 from fastapi.responses import RedirectResponse
 from langchain.chat_models import ChatAnthropic, ChatOpenAI
 from langserve import add_routes
@@ -6,9 +8,11 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from .repository import user_repository
+from .service import user_service
 
 from .model.models import Base
 from .config.mongo import client
+import boto3
 
 from . import schemas
 
@@ -55,6 +59,18 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = user_repository.get_users(db, skip=skip, limit=limit)
     return users
+
+@app.post("/profile/")
+async def update_profile(response: Response, clerk_id: str, profile_picture: UploadFile = File(...)):
+    with open(f"{profile_picture.filename}", "wb") as buffer:
+        shutil.copyfileobj(profile_picture.file, buffer)
+
+    result = user_service.upload_profile(clerk_id, profile_picture.filename)
+    os.remove(profile_picture.filename)
+    if not result:
+        response.status_code = 400
+        return {"message": "Failed to upload profile picture"}
+    return {"message": "Profile picture uploaded successfully"}
 
 
 add_routes(
