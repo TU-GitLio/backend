9e6ed5ba8175681dc8529db511d52c743ee22b77
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-12 22:31:33 +0900

    feat: Create empty Mongo document when create new portfolio

--- gitlio/app/config/mongo.py
+++ gitlio/app/config/mongo.py
@@ -5,10 +5,13 @@ import os
 load_dotenv(find_dotenv())
 MONGODB_ID = os.environ.get("MONGODB_ID")
 MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
+MONGO_DB_NAME = "gitlio"
 
 MONGODB_URL = f"mongodb+srv://{MONGODB_ID}:{MONGODB_PASSWORD}@gitlio.tx0qdtk.mongodb.net/?retryWrites=true&w=majority&appName=gitlio"
 
 client = MongoClient(MONGODB_URL)
+mongo_db = client[MONGO_DB_NAME]
+portfolios_collection = mongo_db["portfolios"]
 
 # Send a ping to confirm a successful connection
 try:
