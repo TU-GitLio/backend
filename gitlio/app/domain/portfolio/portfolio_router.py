1148bf160b4468042ce450d5c593d197df3a6467
Kevin Kim <kevinkim0617kim@gmail.com>
2024-03-13 23:04:41 +0900

    fix: small bug fix

--- gitlio/app/domain/portfolio/portfolio_router.py
+++ gitlio/app/domain/portfolio/portfolio_router.py
@@ -11,7 +11,7 @@ router = APIRouter(
 @router.post("/")
 def create_portfolio_endpoint(portfolio_data: portfolio_schema.PortfolioCreate, db: Session = Depends(get_db)):
     # 도메인 이름 중복 체크
-    db_portfolio = portfolio_crud.get_portfolio_by_domain_name(db, portfolio_data.domainName)
+    db_portfolio = portfolio_crud.get_portfolio_by_domain_name(db, portfolio_data.domain_name)
     if db_portfolio:
         raise HTTPException(status_code=409, detail="Domain name already in use")
     
